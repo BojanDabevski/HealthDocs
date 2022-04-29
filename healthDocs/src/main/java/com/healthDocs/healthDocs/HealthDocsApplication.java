f720aebe6e419d08a89ce62c80417df5676d86a9
Clokwerk <kostov.borjan@gmail.com>
2022-04-29 19:25:57 +0200

    Initial project commit.

---
+++ healthDocs/src/main/java/com/healthDocs/healthDocs/HealthDocsApplication.java
@@ -0,0 +1,13 @@
+package com.healthDocs.healthDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDocsApplication.class, args);
+	}
+
+}
---
+++ healthDocs/src/test/java/com/healthDocs/healthDocs/HealthDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthDocs.healthDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
